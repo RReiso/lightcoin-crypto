4aeb93a5992f46e1e6498a4da771a5d0b20e6431
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-04 00:20:33 +0000

    Withdraw and deposit money. Calculate balance. Use getetr and setter methods

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+    this.balance = 0;
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    return this._balance;
+  }
+
+  set balance(amount) {
+    this._balance = amount;
+  }
+
+  addTransaction(transaction) {
+    const amount = transaction.value;
+    if (this.balance + amount >= 0) {
+      this.transactions.push(transaction);
+      this.balance += amount;
+    } else {
+      console.log("Insufficient funds");
+    }
   }
 
 }
 
+class Transaction {
 
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
 
+  commit() {
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('somebody');
+console.log('Balance:', myAccount.balance);
+console.log("-----------------");
+
+const t1 = new Deposit(120.00, myAccount);
+console.log("deposit: ", t1.amount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Balance:', myAccount.balance);
+console.log("-----------------");
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(100.00, myAccount);
+console.log("withdraw: ", t2.amount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Balance:', myAccount.balance);
+console.log("-----------------");
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(260.00, myAccount);
+console.log("trying to withdraw: ", t3.amount);
+t3.commit();
+console.log('Balance:', myAccount.balance);
+console.log("-----------------");
